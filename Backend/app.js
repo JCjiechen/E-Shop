4b236cf36d0e46370708fb1b3806ec282f71ec48
JC <jc.jiechen.jane@gmail.com>
2022-08-14 00:43:49 -0400

    database set up

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,32 @@
-console.log("hell");
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const productsRounter = require('./routers/products');
+const api = process.env.API_URL;
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+
+// middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+// Routers
+app.use(`${api}/products`, productsRounter);
+
+mongoose.connect(encodeURI(process.env.CONNECTION), {
+    dbName: 'E-Shop'
+})
+    .then(() => {
+        console.log('Database connected!')
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+app.listen(3000, () => {
+    console.log('Server is running http://localhost:3000');
+})
